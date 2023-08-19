25e2690275deaf8283865f997707e2ed6a5ef968
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-08-18 20:28:19 -0400

    Update Sine.MIDI.Interface.h

--- Sine.MIDI.Interface.h
+++ Sine.MIDI.Interface.h
@@ -1,21 +1,68 @@
 #pragma once
 
-// Sine.MIDI.Interface.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Sine.MIDI.Interface.cpp : This file contains the 'main' function. Program execution begins and ends there. //
 
 #include <iostream>
+#include <windows.h>
+#include <string>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    // Enumerate MIDI output devices //
+    unsigned int numDevs = midiOutGetNumDevs();
+
+    if (numDevs == 0) {
+        std::cout << "No MIDI output devices found!" << "\n";
+        return 0;
+    }
+
+    // MIDI output device information //
+    struct midi_object_struct {
+		std::uint64_t nID;
+		MIDIOUTCAPS moc;
+	};
+
+    // Print the names of all MIDI output devices //
+    std::vector<midi_object_struct> devices(numDevs);
+
+    // Enumerate the MIDI output devices //
+    for (unsigned int i = 0; i < numDevs; i++) {
+        MIDIOUTCAPS moc;
+        if (midiOutGetDevCaps(i, &moc, sizeof(MIDIOUTCAPS)) == MMSYSERR_NOERROR) {
+            std::cout << "Device " << i << ": " << &moc.szPname << "\n";
+            midi_object_struct midi_object;
+            midi_object.nID = i;
+            midi_object.moc = moc;
+            devices.push_back(midi_object);
+        }
+    }
+
+    // Open the first MIDI output device //
+    HMIDIOUT hMidiOut = NULL;
+    const MMRESULT& mm_open_const_result = midiOutOpen(&hMidiOut, devices.at(0).nID, 0, 0, CALLBACK_NULL);
+    if (mm_open_const_result != MMSYSERR_NOERROR) {
+        std::cout << "Error opening MIDI output device. Error Code : " << mm_open_const_result << "\n";
+        return 1;
+    }
+
+    // Send a MIDI message (e.g., a Note On for middle C on channel 1) //
+    // 0x90 is Note On for channel 1, 0x3C is middle C, 0x40 is velocity of 64 //    
+    DWORD midiMessage = 0x00403C90;  
+    const MMRESULT& mm_out_const_result = midiOutShortMsg(hMidiOut, midiMessage);
+    if (mm_out_const_result != MMSYSERR_NOERROR) {
+        std::cout << "Error sending MIDI message. Error Code: " << mm_out_const_result << "\n";
+        return 1;
+    }
+
+    // Let the note play for 1.5 seconds //
+    Sleep(1500);  
+
+    // Close the MIDI output device //
+    midiOutClose(hMidiOut);
+
+    // Wait for user input //
+    std::string ch{};
+    std::cin >> ch;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
