776298d3be852639947ea650fa895ca9e1f17bb1
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-08-30 17:57:42 -0400

    Update Sine.MIDI.Interface.Out.h

--- Sine.MIDI.Interface.Out.h
+++ Sine.MIDI.Interface.Out.h
@@ -48,28 +48,39 @@ int main() {
     }
 
     // Open the first MIDI output device //
-    HMIDIOUT hMidiOut = NULL;
-    const MMRESULT& mm_open_const_result = midiOutOpen(&hMidiOut, devices.at(0).nID, 0, 0, CALLBACK_NULL);
-    if (mm_open_const_result != MMSYSERR_NOERROR)
-    {
-        char error_msg[MAXERRORLENGTH];
-        midiOutGetErrorTextA(mm_open_const_result, error_msg, static_cast<UINT>( sizeof(error_msg) / sizeof(char) ));
-        std::cout << "Error opening MIDI output device. Error Code : " << mm_open_const_result << ", Message: " << error_msg << "\n";
-        return 1;
-    }
+    HMIDIOUT hMidiOut{};
+
+    // Open the MIDI output device //
+    MMRESULT mm_open_const_result{};
+    do {
+        mm_open_const_result = midiOutOpen(&hMidiOut, devices.at(0).nID, 0, 0, CALLBACK_NULL);
+        if (mm_open_const_result == MMSYSERR_NOERROR)
+        {
+            //char error_msg[MAXERRORLENGTH];
+            //midiOutGetErrorTextA(mm_open_const_result, error_msg, static_cast<UINT>( sizeof(error_msg) / sizeof(char) ));
+            //std::cout << "Error opening MIDI output device. Error Code : " << mm_open_const_result << ", Message: " << error_msg << "\n";
+            std::cout << "SUCCESS opening MIDI output device : " << devices.at(0).nID << "\n";
+            break;
+        }
+    } while (mm_open_const_result != MMSYSERR_NOERROR);
 
     // Send a MIDI message (e.g., a Note On for middle C on channel 1) //
-    // 0x90 is Note On for channel 1, 0x3C is middle C, 0x40 is velocity of 64 //    
-    DWORD midiMessage = 0x00403C90;  
-    const MMRESULT& mm_out_const_result = midiOutShortMsg(hMidiOut, midiMessage);
-    if (mm_out_const_result != MMSYSERR_NOERROR)
-    {
-        std::cout << "Error sending MIDI message. Error Code: " << mm_out_const_result << "\n";
-        return 1;
-    }
+    // 0x90 is Note On for channel 1, 0x3C is middle C, 0x40 is velocity of 64 //
+    MMRESULT mm_out_const_result{};
+    DWORD midiMessage = 0x00403C90;
+    do {
+        mm_out_const_result = midiOutShortMsg(hMidiOut, midiMessage);
+        if (mm_out_const_result == MMSYSERR_NOERROR)
+        {
+            //std::cout << "Error sending MIDI message. Error Code: " << mm_out_const_result << "\n";
+            std::cout << "SUCCESS sending MIDI message : " << midiMessage << "\n";
 
-    // Let the note play for 1.5 seconds //
-    Sleep(1500);  
+            // Let the note play for 1.5 seconds //
+            Sleep(1500);
+
+            break;
+        }
+    } while (mm_out_const_result != MMSYSERR_NOERROR);
 
     // Close the MIDI output device //
     midiOutClose(hMidiOut);
